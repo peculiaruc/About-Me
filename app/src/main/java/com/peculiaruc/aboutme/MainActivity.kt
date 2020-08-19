551233d05e44be41a9aabcaa5efe4958ef8d832f
pecpaker <umehpeculiar20@gmail.com>
2020-08-20 00:01:47 +0100

    update

--- app/src/main/java/com/peculiaruc/aboutme/MainActivity.kt
+++ app/src/main/java/com/peculiaruc/aboutme/MainActivity.kt
@@ -12,10 +12,30 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
+
+
     }
+
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_editText)
+        val textView = findViewById<TextView>(R.id.nickname_textview)
+
+        textView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        textView.visibility = View.VISIBLE
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+
     }
+}
--- app/src/main/java/com/peculiaruc/aboutme/WelcomeActivity.kt
+++ app/src/main/java/com/peculiaruc/aboutme/WelcomeActivity.kt
@@ -1,6 +1,7 @@
 package com.peculiaruc.aboutme
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,41 +9,34 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
-        findViewById<Button>(R.id.done_button).setOnClickListener {
-            addNickname(it)
-        }
-        fun addNickname(view: View) {
-//            val editText = findViewById<EditText>(R.id.nickname_editText)
-//            val textView = findViewById<TextView>(R.id.nickname_textview)
-//
-//            textView.text =editText.text
-//            editText.visibility = View.GONE
-//            view.visibility = View.GONE
-//            textView.visibility = View.VISIBLE
-//
-//            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-//            imm.hideSoftInputFromWindow(view.windowToken, 0)
+//        findViewById<Button>(R.id.btn_check).setOnClickListener {
+//           addNickname(it)
+//        }
+        btn_check.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 
-    private fun addNickname(view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_editText)
-        val textView = findViewById<TextView>(R.id.nickname_textview)
-
-        textView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        textView.visibility = View.VISIBLE
-
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
-
-    }
+//    private fun addNickname(view: View) {
+//        val editText = findViewById<EditText>(R.id.nickname_editText)
+//        val textView = findViewById<TextView>(R.id.nickname_textview)
+//
+//        textView.text = editText.text
+//        editText.visibility = View.GONE
+//        view.visibility = View.GONE
+//        textView.visibility = View.VISIBLE
+//
+//        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+//        imm.hideSoftInputFromWindow(view.windowToken, 0)
+//
+//    }
 
 }
