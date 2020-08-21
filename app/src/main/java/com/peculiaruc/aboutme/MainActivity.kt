10665f9492175e9f4621c702c5a48705e55ac95d
pecpaker <umehpeculiar20@gmail.com>
2020-08-21 14:51:53 +0100

    update

--- app/src/main/java/com/peculiaruc/aboutme/MainActivity.kt
+++ app/src/main/java/com/peculiaruc/aboutme/MainActivity.kt
@@ -8,31 +8,39 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.peculiaruc.aboutme.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
 
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+//        findViewById<Button>(R.id.done_button).setOnClickListener {
+//            addNickname(it)
+//        }
+
+        binding.doneButton.setOnClickListener {
             addNickname(it)
         }
-
 
     }
 
     private fun addNickname(view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_editText)
-        val textView = findViewById<TextView>(R.id.nickname_textview)
 
-        textView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        textView.visibility = View.VISIBLE
+        binding.apply {
+            binding.nicknameTextview.text = nickname_editText.text
+            invalidateAll()
+            binding.nicknameEditText.visibility = View.GONE
+            binding.doneButton.visibility = View.GONE
+            binding.nicknameTextview.visibility = View.VISIBLE
+
+        }
 
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
